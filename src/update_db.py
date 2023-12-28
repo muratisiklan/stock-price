7b7a4f21dfe857aa5068a725c5f3d0ad270db10e
muratisiklan <muratisiklan@hotmail.com>
2023-12-28 23:36:01 +0300

    document format changed, new currencies added, minor bugs fixed

--- src/update_db.py
+++ src/update_db.py
@@ -2,85 +2,62 @@ import pandas as pd
 from pymongo import MongoClient
 from datetime import datetime, timedelta
 from src.utils import get_stock_data
+import yfinance as yf
 """
     Update Database (Load New Data)
 """
+    
 
+def update_database(collection_name):
+    # Constants
+    MONGO_URI = "mongodb://127.0.0.1:27017/"
+    DB_NAME = "stockdata"
+    COLLECTION_NAME = collection_name
+    bist_data = pd.read_csv(
+        "/Users/muratisiklan/Desktop/stock-price/Artifacts/bist_data.csv")
 
-def update_db_sp500():
-    # Connect to MongoDB (replace 'your_mongodb_uri' and 'your_database_name')
-    with MongoClient('mongodb://127.0.0.1:27017/') as client:
-        db = client['stockdata']
-        collection = db['stock']
+    # Connect to MongoDB
+    with MongoClient(MONGO_URI) as client:
+        db = client[DB_NAME]
+        collection = db[COLLECTION_NAME]
 
-        # Read S&P 500 company data from the CSV file
-        sp500_df = pd.read_csv("/Users/muratisiklan/Desktop/stock-price/Artifacts/constituents_csv.csv")
+        # Get the most recent date in the database
+        most_recent_entry = collection.find_one({}, sort=[("_id", -1)])
 
-        # Get the tickers, company names, and create a dictionary for quick lookup
-        sp500_data = sp500_df.set_index('Symbol')[['Name']].to_dict()['Name']
+        if most_recent_entry:
+            most_recent_date = datetime.strptime(
+                most_recent_entry["_id"], "%Y-%m-%d")
+        else:
+            # If the database is empty, set the most recent date to a specific start date
+            most_recent_date = datetime.strptime("2015-01-01", "%Y-%m-%d")
 
-        # Take the top 500 companies
-        top_500_symbols = sp500_df.head(500)['Symbol']
+        # Calculate the date range to fetch data
+        today = datetime.now()
+        date_range_start = (most_recent_date).strftime("%Y-%m-%d")
+        date_range_end = (today+timedelta(days=1)).strftime("%Y-%m-%d")
 
-        # Find the most recent date in the MongoDB collection for any symbol
-        latest_date_record = collection.find_one(sort=[('Date', -1)])
-        latest_date = latest_date_record['Date'] if latest_date_record else datetime(2000, 1, 1)
+        for index, row in bist_data.iterrows():
+            symbol = row['Symbol']
+            company = row['Name']
 
-        # Date range for the most recent data until now
-        start_date = (latest_date + timedelta(days=1)).strftime('%Y-%m-%d')
-        end_date = datetime.now().strftime('%Y-%m-%d')
+            # Fetch stock data
+            stock_data = yf.download(
+                symbol, start=date_range_start, end=date_range_end)
 
-        # Iterate through top 500 symbols
-        for symbol in top_500_symbols:
-            data = get_stock_data(symbol, start_date, end_date)
-
-            if data is not None:
-                # Add 'Symbol' and 'Company' to each record
-                data['Symbol'] = symbol
-                data['Company'] = sp500_data.get(symbol, '')
-
-                # Reset the index and convert to a list of dictionaries for insertion
-                data_dict_list = data.reset_index().to_dict(orient='records')
-
-                # Check if the list is not empty before insertion
-                if data_dict_list:
-                    # Insert the data into MongoDB
-                    collection.insert_many(data_dict_list)
-
-def update_db_bist():
-    # Connect to MongoDB (replace 'your_mongodb_uri' and 'your_database_name')
-    with MongoClient('mongodb://127.0.0.1:27017/') as client:
-        db = client['stockdata']
-        collection = db['bist']
-
-        # Read S&P 500 company data from the CSV file
-        bist_df = pd.read_csv("/Users/muratisiklan/Desktop/stock-price/Artifacts/bist_data.csv")
-
-        bist_data = bist_df.set_index('Code')[['Name']].to_dict()['Name']
-
-        top_500_symbols = bist_df.head(500)['Code']
-
-        # Find the most recent date in the MongoDB collection for any symbol
-        latest_date_record = collection.find_one(sort=[('Date', -1)])
-        latest_date = latest_date_record['Date'] if latest_date_record else datetime(2000, 1, 1)
-
-        # Date range for the most recent data until now
-        start_date = (latest_date + timedelta(days=1)).strftime('%Y-%m-%d')
-        end_date = datetime.now().strftime('%Y-%m-%d')
-
-        # Iterate through top 500 symbols
-        for symbol in top_500_symbols:
-            data = get_stock_data(symbol, start_date, end_date)
-
-            if data is not None:
-                # Add 'Symbol' and 'Company' to each record
-                data['Code'] = symbol
-                data['Company'] = bist_data.get(symbol, '')
-
-                # Reset the index and convert to a list of dictionaries for insertion
-                data_dict_list = data.reset_index().to_dict(orient='records')
-
-                # Check if the list is not empty before insertion
-                if data_dict_list:
-                    # Insert the data into MongoDB
-                    collection.insert_many(data_dict_list)
+            if not stock_data.empty:
+                # Organize stock data by date
+                for date_str, data in stock_data.iterrows():
+                    date_str = date_str.strftime('%Y-%m-%d')
+                    document = {
+                        "Symbol": symbol,
+                        "Company": company,
+                        "Open": data['Open'],
+                        "High": data['High'],
+                        "Low": data['Low'],
+                        "Close": data['Close'],
+                        "AdjClose": data['Adj Close'],
+                        "Volume": data['Volume']
+                    }
+                    # Insert document into MongoDB collection
+                    collection.update_one({"_id": date_str}, {
+                                          "$push": {"bist": document}}, upsert=True)
