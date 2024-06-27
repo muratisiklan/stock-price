105004bf8cba8bdd73973595b1153eda9de227ab
muratisiklan <muratisiklan@hotmail.com>
2024-06-27 11:17:45 +0300

    init frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Student from './components/Student.jsx'
+
+
+
+function App() {
+
+  return(
+    <>
+      <Student name="random" age = {30} isStudent = {false}/>
+      <Student name="random" age = {30} isStudent = {false}/>
+    
+    
+    
+    </>
+  );
+}
+
+export default App
