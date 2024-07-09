ba8b6b47fffe118d044f984ac4ea7bd873c0120d
muratisiklan <muratisiklan@hotmail.com>
2024-07-09 22:26:55 +0300

    integration and delivery pipeline

--- backend/app/routers/analytics.py
+++ backend/app/routers/analytics.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Query
 from sqlalchemy.orm import Session, aliased
-from datetime import datetime,date
+from datetime import datetime, date
 from sqlalchemy import func, distinct
 from dateutil.relativedelta import relativedelta
 from typing import Optional
@@ -10,11 +10,11 @@ from ..models import User, Divestment, Investment
 from ..schemas.analytics_schema import AnalyticsResponse, CompanyDetail
 from .auth import get_current_user
 
+import requests
 # Analytics calculations are based on transactions(both investment and divestment) made in specified date interval
 # if a divestment is included in specified interval; yet associated investment is before that date; such
 # divestment is not accounted while calculated analytics
 # ie: Analytics calculated for Investments(and associated divestments) made inside specified time interval
-
 
 
 router = APIRouter(prefix="/analytics", tags=["analytics"])
@@ -27,12 +27,11 @@ db_dependency = Depends(get_db)
 async def get_data_last_month(
     db: Session = db_dependency,
     user: dict = user_dependency,
-    last_month_start:Optional[date] = datetime.today().date()
+    last_month_start: Optional[date] = datetime.today().date()
 ):
     if user is None:
         raise HTTPException(status_code=401, detail="Authentication Failed!")
 
- 
     # Query for investments
     investment_data = db.query(
         func.count(Investment.id).label("num_investments"),
@@ -41,7 +40,8 @@ async def get_data_last_month(
         func.count(distinct(Investment.company)).label(
             "distinct_companies_invested"),
         func.sum(Investment.quantity).label("quantity_invested"),
-        func.sum(Investment.quantity_remaining).label("quantity_nonrealized_investment"),
+        func.sum(Investment.quantity_remaining).label(
+            "quantity_nonrealized_investment"),
     ).filter(
         Investment.owner_id == user["id"],
         Investment.date_invested >= last_month_start
@@ -55,7 +55,8 @@ async def get_data_last_month(
         func.count(distinct(Divestment.company)).label(
             "distinct_companies_divested"),
         func.sum(Divestment.quantity).label("quantity_divested"),
-        func.sum(Divestment.cost_of_investment).label("cost_of_realized_investment"),
+        func.sum(Divestment.cost_of_investment).label(
+            "cost_of_realized_investment"),
         func.sum(Divestment.revenue).label("revenue_from_realized_investment"),
         func.sum(Divestment.net_return).label("net_return")
     ).filter(
@@ -70,7 +71,8 @@ async def get_data_last_month(
         func.sum(Investment.quantity *
                  Investment.unit_price).label("total_invested"),
         func.sum(Investment.quantity).label("quantity_invested"),
-        func.sum(Investment.quantity_remaining).label("quantity_nonrealized_investment")
+        func.sum(Investment.quantity_remaining).label(
+            "quantity_nonrealized_investment")
     ).filter(
         Investment.owner_id == user["id"],
         Investment.date_invested >= last_month_start
@@ -82,7 +84,8 @@ async def get_data_last_month(
         func.sum(Divestment.quantity *
                  Divestment.unit_price).label("total_divested"),
         func.sum(Divestment.quantity).label("quantity_divested"),
-        func.sum(Divestment.cost_of_investment).label("cost_of_realized_investment"),
+        func.sum(Divestment.cost_of_investment).label(
+            "cost_of_realized_investment"),
         func.sum(Divestment.revenue).label("revenue_from_realized_investment"),
         func.sum(Divestment.net_return).label("net_return")
     ).filter(
@@ -123,8 +126,8 @@ async def get_data_last_month(
             quantity_invested=row.quantity_invested or 0,
             quantity_nonrealized_investment=row.quantity_nonrealized_investment or 0,
             quantity_divested=row.quantity_divested or 0,
-            cost_of_realized_investment= row.cost_of_realized_investment or 0,
-            revenue_from_realized_investment= row.revenue_from_realized_investment or 0,
+            cost_of_realized_investment=row.cost_of_realized_investment or 0,
+            revenue_from_realized_investment=row.revenue_from_realized_investment or 0,
             net_return=row.net_return or 0.0
         )
         company_details.append(company_detail)
@@ -139,12 +142,28 @@ async def get_data_last_month(
         total_invested=investment_data.total_invested or 0.0,
         total_divested=divestment_data.total_divested or 0.0,
         quantity_invested=investment_data.quantity_invested or 0,
-        quantity_nonrealized_investment = investment_data.quantity_nonrealized_investment or 0,
+        quantity_nonrealized_investment=investment_data.quantity_nonrealized_investment or 0,
         quantity_divested=divestment_data.quantity_divested or 0,
-        cost_of_realized_investment = divestment_data.cost_of_realized_investment or 0,
-        revenue_from_realized_investment= divestment_data.revenue_from_realized_investment or 0,
+        cost_of_realized_investment=divestment_data.cost_of_realized_investment or 0,
+        revenue_from_realized_investment=divestment_data.revenue_from_realized_investment or 0,
         net_return=divestment_data.net_return or 0.0,
         investments_by_company=company_details
     )
+    url = 'http://stock-price-mlservice-1:8080/metrics/'  # Use the service name
+    params = {
+        'symbol': 'ASELS.IS',
+        'start_date': '2024-01-01'
+    }
+
+    try:
+        res = requests.get(url, params=params)
+        res.raise_for_status()  # Raise an exception for HTTP errors (4xx or 5xx)
+
+        # Process the response
+        data = res.json()  # Assuming the response is JSON
+        print(data)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error sending request: {e}")
 
     return response
