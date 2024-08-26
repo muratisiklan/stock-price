9a9e1714c5e6beecd40e5f56a21b7465bc1cef59
muratisiklan <muratisiklan@hotmail.com>
2024-08-26 12:26:39 +0300

    recommender router init

---
+++ mlservice/app/routers/recommendation.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Depends, HTTPException, status, Query
+from datetime import datetime, date
+from typing import Optional
+from ..components.stage02get_data import CompanyMetrics
+from ..database import mongo_uri
+from ..utils.utils import sanitize_for_json
+
+router = APIRouter(prefix="/recommendation", tags=["recommendation"])
+
+
+@router.get("/",  status_code=status.HTTP_200_OK)
+async def get_company_metrics(
+    symbol: str = Query(default="ASELS.IS",
+                        description="Stock symbol of the company"),
+
+):
+
+    #TODO: Fetch data for every company with close prices
+
+
+    #TODO: calculate additional metrics for companies
+
+    #Todo: İmpute missing values etc (data cleaning)
+
+
+    #TODO: create clustering with optimal K clusters
+
+
+    #TODO: Randomly select 3 company where symbol is in same cluster
+
+    #TODO: Return selected 3 company symbols as recommendation
+
+
+    return None
